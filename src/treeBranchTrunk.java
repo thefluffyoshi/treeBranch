c1369e98545bb34025c2fc4d5d5e976bb030e1ce
thefluffyoshi <katielynntrinh@gmail.com>
2020-03-09 13:23:27 -0700

    Added Mr. Beast's brag

--- src/treeBranchTrunk.java
+++ src/treeBranchTrunk.java
@@ -3,5 +3,6 @@ public class treeBranchTrunk
     public static void main(String[] args)
     {
         System.out.println("The trees are dying.");
+        System.out.println("Mr Beast planted 20,000,000 trees.");
     }//end main method
 }//end driver class
